124abf3fbdb3449fd8944e82cce00112f49e130e
PanjiSri <neiabaraja04@gmail.com>
2024-05-24 15:10:44 +0700

    saver

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -6,16 +6,20 @@ package org.example;
 // import javafx.scene.Scene;
 // import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import plugin.TXTLoader;
 import plugin.TXTSaver;
-import java.util.Scanner;
-import java.util.List;
 
 public class MainApp{
 
     List<String> gameState;
     List<String> player1;
     List<String> player2;
+
+
 
 //     @Override
 //     public void start(Stage primaryStage) throws Exception {
@@ -55,13 +59,21 @@ public class MainApp{
         this.player1 = loader.tokenizeLines(loader.readFromFile("player1.txt"));
         this.player2 = loader.tokenizeLines(loader.readFromFile("player2.txt"));
 
-        System.out.println(gameState);
+        // System.out.println(player2);
 
-        List<String> formatted = loader.repairFormat(gameState);
+        // System.out.println(formatPlayerData(player2));
+        
+        List<String> asli = loader.readFromFile("player2.txt");
 
-        System.out.println(formatted);
+        System.out.println(asli);
 
-        saver.saveFormattedData("data_game", "ngopi.txt", formatted);
+        // List<String> formatted = loader.repairFormat(gameState);
+
+        // System.out.println(formatted);
+
+        // saver.saveFormattedData("data_game", "ngopi.txt", formatted);
+
+        saver.saveFormattedData("ngopi", "ngopi.txt", asli);
 
         scanner.close();
     }
@@ -70,6 +82,41 @@ public class MainApp{
         // launch(args);
         MainApp m = new MainApp();
         m.loadTXT();
+    }
+
+    public List<String> formatPlayerData(List<String> dataList) {
+        List<String> formattedData = new ArrayList<>();
+
+        if (dataList.size() > 2) {
+            formattedData.add(dataList.get(0)); 
+            formattedData.add(dataList.get(1)); 
+            formattedData.add(dataList.get(2)); 
+        }
+
+        StringBuilder currentEntry = new StringBuilder();
+        for (int i = 3; i < dataList.size(); i++) {
+            String item = dataList.get(i).trim();
+            boolean isNumeric = item.matches("^[0-9]+$");
+
+            // Start a new line if it's a card location or numeric (indicating a new group) and currentEntry is not empty
+            if ((item.matches("^[AB][0-9]{2}$") || isNumeric) && currentEntry.length() > 0) {
+                formattedData.add(currentEntry.toString().trim());
+                currentEntry.setLength(0); // Reset the StringBuilder
+            }
+
+            if (currentEntry.length() > 0) {
+                currentEntry.append(" ");
+            }
+            currentEntry.append(item);
+        }
+
+        // Add the last entry if it's not empty
+        if (currentEntry.length() > 0) {
+            formattedData.add(currentEntry.toString());
+        }
+
+        return formattedData;
+    
     }
     
 }
